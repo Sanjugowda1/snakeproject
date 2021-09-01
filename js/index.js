a56beaf706bb1de77c5d855f2d76be6a1d71cc7c
Sanjugowda1 <87930774+Sanjugowda1@users.noreply.github.com>
2021-09-01 21:29:03 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let inputDir = {x: 0, y: 0};
 const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
-const musicSound = new Audio('https://www.youtube.com/watch?v=cRGXkP4dZCU');
+const musicSound = new Audio('music/bhoomi.mp3');
 let speed = 6;
 let score = 0;
 let lastPaintTime = 0;
